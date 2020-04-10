804267f73705b845e6441e47b14a1570f5f8e1bf
Verneri Turunen <vili.turunen@aalto.fi>
2020-04-10 18:28:26 +0300

    added some user interaction to main

--- Labyrintti_Classes/main.py
+++ Labyrintti_Classes/main.py
@@ -12,9 +12,30 @@ from gui import GUI
 
 
 def main():
+    print("*** Welcome to the Labyrinth! ***\n")
+    player = input("Type in your username: ")
+    if len(player) >0:
+        mouse_symbol = player[0] #Mouse symbol will be username's first symbol -> switched to uppercase if possible
+        
+        mouse_symbol = mouse_symbol.upper()
+    else:
+        mouse_symbol = 'M' #if user didn't enter anything, default symbol will be 'M'
+    print("Mouse symbol: {}".format(mouse_symbol))
+    play = False
+    while not play:
+        try:
+            width = int(input("Choose Labyrinth width: "))
+            height = int(input("Choose Labyrinth height: "))
+            if width > 0 and height > 0:
+                play = True
+            else:
+                print("\nEnter positive numbers larger than 0!\n")
+        except ValueError:
+            print("\nEnter positive numbers larger than 0!\n")
+        
    
         
-    test_maze = Maze(10,10)
+    test_maze = Maze(width,height)
     print(test_maze)
     test_maze.carve_maze()
     print(test_maze)
