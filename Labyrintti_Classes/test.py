c408e0189090570e9a3788aede08e73ef34afb4d
Verneri Turunen <vili.turunen@aalto.fi>
2020-05-05 16:24:50 +0300

    adding unittest class

---
+++ Labyrintti_Classes/test.py
@@ -0,0 +1,22 @@
+import unittest
+from io import StringIO
+from square import Square
+from maze import Maze
+from gui import GUI
+
+
+
+
+class Test(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(1, 1)
+    
+    
+
+        
+        
+
+unittest.main()
+        
+
