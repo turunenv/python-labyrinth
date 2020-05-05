09ec708db13a57ac3c16af55b8aa41db84dd1a22
Verneri Turunen <vili.turunen@aalto.fi>
2020-05-05 17:13:35 +0300

    added test-class to test main functionalities

--- Labyrintti_Classes/test.py
+++ Labyrintti_Classes/test.py
@@ -1,16 +1,48 @@
 import unittest
 from io import StringIO
-from square import Square
 from maze import Maze
+from square import Square
 from gui import GUI
+from corrupted_maze_file_error import CorruptedMazeFileError
+
 
 
 
 
 class Test(unittest.TestCase):
     
-    def test_optimal_solution(self):
-        self.assertEqual(1,1)
+    #test functions with working 10x10 maze
+    
+
+
+    def test_maze_features(self):
+
+            
+            try:
+                self.maze1 = Maze.load_from_file("maze.txt")
+            except CorruptedMazeFileError:
+                self.fail("Loading correctly structured file caused an exception.")
+                
+            self.assertEqual(self.maze1.find_shortest_path(),"WWWWSESSSWSESSSEEEE")
+            self.assertEqual(self.maze1.ask_for_tip(),"WWW")
+            self.assertEqual(self.maze1.get_mouse_symbol(),"V")
+            self.assertEqual(self.maze1.get_mouse_square(),self.maze1.get_square(8,1))
+            
+            #test that there is only one mouse
+            count = 0
+            for y in range (self.maze1.height):
+                for x in range (self.maze1.width):
+                    if self.maze1.get_square(x,y).mouse:
+                        count +=1
+            self.assertEqual(1,count)
+            
+    def test_squares(self):
+            try:
+                self.maze1 = Maze.load_from_file("maze.txt")
+            except CorruptedMazeFileError:
+                self.fail("Loading correctly structured file caused an exception.")
+                
+                
     
     
     
